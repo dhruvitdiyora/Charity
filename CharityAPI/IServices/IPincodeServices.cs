be3db6866cfa7b6c3643e0bce2717a307d610df2
piyusht57 <67514560+piyusht57@users.noreply.github.com>
2021-12-09 22:58:54 +0530

    IPost,IPincode and IUrgency services is added

---
+++ CharityAPI/IServices/IPincodeServices.cs
@@ -0,0 +1,24 @@
+﻿using CharityAPI.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CharityAPI.IServices
+{
+    public interface IPincodeServices
+    {
+        public IEnumerable GetAllPincode();
+        public IEnumerable GetPincodeByPincodeId(int PincodeId);
+        public IEnumerable GetPincodeByCityId(int CityId);
+        public IEnumerable GetPincodeByStateId(int StateId);
+        public IEnumerable GetPincodeByPostOfficeName(string PostofficeName);
+        public IEnumerable GetPincodeByDistric(string District);
+        public IEnumerable GetDistrictByPincodeId(int PincodeId);
+        public void AddPincode(Pincode pincode);
+        public void EditPincode(Pincode pincode);
+        public void DeletePincode(int PincodeId);
+
+    }
+}
